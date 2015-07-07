703657dcf60e568f10501982527818bf6ad7f4ff
marco <salerno.ma88@gmail.com>
2015-07-07 12:26:50 +0200

    mysql_fetch_assoc

--- elimina.php
+++ elimina.php
@@ -1,10 +1,11 @@
 <?php
 //carico init
-include_once("init.php");
+include_once("init.php"); 
 include_once("connessione.php");
+$id = $_GET['id'];
 
 $query = "DELETE FROM utenti WHERE id=$id";
-$ris = mysql_query($query, $link);
-echo 'utente eliminato, torna indietro';
-header("Refresh: 4; url=lista_utenti.php");
+ $ris = mysql_query($query, $link);
+     echo 'utente eliminato, torna indietro';
+     header("Refresh: 4; url=lista_utenti.php");
 ?>
--- lista_utenti.php
+++ lista_utenti.php
@@ -7,40 +7,30 @@ include_once("connessione.php");
       
 //leggo tutti i dati della tabella
 $query = "SELECT * FROM utenti";
- $ris = mysql_query($query, $link) or die (mysql_error());
-$num = mysql_numrows($ris); 
- ?>
+$ris = mysql_query($query, $link) or die (mysql_error());
+?>
+
 <!--tabella di dati -->
- <table border="0" cellspacing="2" cellpadding="2">
- <tr>
- <th>Nome</font></th>
- <th>Cognome</font></th>
-<th>Email</font></th>
- <th>Id</font></th>
- </tr>
+<table border="0" cellspacing="2" cellpadding="2">
+<tr>
+    <th>Nome</font></th>
+    <th>Cognome</font></th>
+    <th>Email</font></th>
+    <th>Id</font></th>
+</tr>
 
  <!-- estrapolazione dei dati all'interno di $num-->
  <?php
- while ($riga = mysql_fetch_assoc($num)) {
-        echo $riga["nome"];
-        echo $riga["cognome"];
-        echo $riga["email"];
-echo $riga["id"];
+     while ($riga = mysql_fetch_assoc($ris)) { ?> 
+        <tr>
+        <td><?php echo $riga["nome"];?> </td>
+        <td><?php echo $riga["cognome"];?></td>
+        <td><?php echo $riga["email"];?></td>
+        <td><?php echo $riga["id"];?></td>
+        <?php echo "<td><a href='dettaglio.php?id=$riga[id]'>modifica</td></a>";?>
+        <?php echo "<td><a href='elimina.php?id=$riga[id]'>elimina</td></a>";}?>
+        </tr>
 
-    }
- ?>
- <!--inserimento dei dati estrapolati-->
-  <tr>
-     <td><?php echo $riga["nome"];?>
-     <td><?php echo $riga["cognome"];?>
-     <td><?php echo $riga["email"];?>
-     <td><?php echo $riga["id"];?></td>
-      $idd = $riga["id"]
-<?php echo "<td><a href='dettaglio.php?id=$idd'>modifica</td></a>";?>
-<?php echo "<td><a href='elimina.php?id=$idd'>elimina</td></a>";?>
-  </tr>
-
-
- <div id="logoutButton">
-        <a href="logout.php">logout</a>
-    </div>
+<div id="logoutButton">
+    <a href="logout.php">logout</a>
+</div>
