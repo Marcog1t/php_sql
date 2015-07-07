df5c256b97518d9c7e87a221a548abc6613fd7d4
marco <salerno.ma88@gmail.com>
2015-07-07 11:08:38 +0200

    localhost

--- localhost.sql
+++ localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.7
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Lug 06, 2015 alle 15:19
--- Versione del server: 5.1.71-community-log
--- PHP Version: 5.3.10
+-- Host: 127.0.0.1
+-- Generation Time: Lug 07, 2015 alle 11:07
+-- Versione del server: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `my_marcowebwork`
+-- Database: `marco`
 --
 
 -- --------------------------------------------------------
@@ -28,22 +28,28 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `utenti` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `cognome` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `nome` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cognome` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dump dei dati per la tabella `utenti`
 --
 
 INSERT INTO `utenti` (`id`, `nome`, `cognome`, `email`) VALUES
-(1, 'prova', 'prova', 'prova@prova.it'),
-(2, 'Marco', 'Salerno', ''),
-(7, 'sam', 'sung', 'sam@apple.it'),
-(6, 'marco', 'salerno', 'sddsds'),
-(5, 'marco', 'salerno', 'salerno.ma88@gmail.com');
+(1, 'aldo', 'baglio', ''),
+(9, 'nè', 'ciò', 'ciao@defr.it'),
+(10, 'valentino', 'rossi', ''),
+(11, 'marco', 'salerno', ''),
+(12, 'b', 'c', ''),
+(13, 'provaìèà', 'provaò', 'provà@prova.it'''),
+(16, 'prova', 'prova', ''),
+(17, 'marco2', 'prova2', ''),
+(18, 'marcoì', 'marco''', 'marco''@tiko.it'),
+(19, 'marco''', '', ''),
+(20, 'marco', '', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
